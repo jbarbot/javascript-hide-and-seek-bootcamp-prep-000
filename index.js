a254a33667029fb6a1682aadf52c9edc917fddee
Jacob Barbot <barbotjacob@gmail.com>
2018-06-30 12:29:34 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,30 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+
+
+function nestedTarget() {
+ return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  const rankedLists = document.querySelectorAll('.ranked-list')
+  
+  for (i = 0; i < rankedLists.length, i++)
+  
+}
+
+
+function deepestChild() {
+  var node = document.getElementById('grand-node');
+  let nextNode = node.children[0];
+  
+ while (nextNode) {
+    node = nextNode
+    nextNode = node.children[0]
+   
+ }
+return node
+}
+  
