6045767ad1325cf8f6916155918fc9695f1b5fde
Jacob Barbot <barbotjacob@gmail.com>
2018-06-30 12:54:58 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,36 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+
+
+function nestedTarget() {
+ return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  const rankedLists = document.querySelectorAll('.ranked-list')
+  
+  for (let i = 0; i < rankedLists.length; i++) {
+    let x = rankedLists[i].children
+    
+    for (let y = 0; y < x.length; y++) {
+      x[y].innerHTML = parseInt(x[y].innerHTML) + n
+    }
+    
+  }
+}
+
+
+function deepestChild() {
+  var x = document.getElementById('grand-node');
+  let y = x.children[0];
+  
+ while (y) {
+    x = y
+   y = x.children[0]
+   
+ }
+return x
+}
+  
